084e7d4437a510ae60fcbb92bd87bd6b0969e5ae
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2023-12-20 03:05:59 +0300

    Ready algorithm

--- deltaver/version_delta.py
+++ deltaver/version_delta.py
@@ -1,6 +1,14 @@
+import datetime
+
 import attrs
+import httpx
+from packaging import version
 
 from typing import Protocol
+
+
+class VersionNotFoundError(Exception):
+    pass
 
 
 class VersionDelta(Protocol):
@@ -16,4 +24,33 @@ class PypiVersionDelta(VersionDelta):
     _version: str
 
     def days(self):
-        return 1
+        response = httpx.get('https://pypi.org/pypi/{0}/json'.format(self._package_name))
+        response.raise_for_status()
+        available_release_versions = sorted(
+            [
+                {version_number: release_info}
+                for version_number, release_info in response.json()['releases'].items()
+                if not version.parse(version_number).pre
+            ],
+            key=lambda release_dict: version.parse(list(release_dict.keys())[0]),
+        )
+        if list(available_release_versions[-1].keys())[0] == self._version:
+            return 0
+        start = None
+        next_available_release = None
+        flag = False
+        for release_number, _ in response.json()['releases'].items():
+            if flag:
+                next_available_release = release_number
+                break
+            if release_number == self._version:
+                flag = True
+        for version_info, release in response.json()['releases'].items():
+            if release == []:
+                continue
+            upload_time = datetime.datetime.strptime(release[0]['upload_time'], '%Y-%m-%dT%H:%M:%S').date()
+            if version_info == next_available_release:
+                start = upload_time
+        if not start:
+            raise VersionNotFoundError
+        return (datetime.datetime.now().date() - start).days
--- tests/unit/test_version_delta.py
+++ tests/unit/test_version_delta.py
@@ -1,5 +1,26 @@
+import respx
+from pathlib import Path
+from httpx import Response
+
 from deltaver.version_delta import PypiVersionDelta
 
 
-def test():
-    assert PypiVersionDelta('', '').days() == 1
+@respx.mock
+def test_previous(time_machine):
+    # 0.25.1 was released 2023-11-03
+    # 0.25.2 was released 2023-11-24
+    time_machine.move_to('2023-12-20')
+    respx.get('https://pypi.org/pypi/httpx/json').mock(return_value=Response(
+        200,
+        text=Path('tests/fixtures/httpx_pypi_response.json').read_text(),
+    ))
+    assert PypiVersionDelta('httpx', '0.25.1').days() == 26
+
+
+@respx.mock
+def test_last_version():
+    respx.get('https://pypi.org/pypi/httpx/json').mock(return_value=Response(
+        200,
+        text=Path('tests/fixtures/httpx_pypi_response.json').read_text(),
+    ))
+    assert PypiVersionDelta('httpx', '0.25.2').days() == 0
