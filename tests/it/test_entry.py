2a85d55a106683004c9eb36a843e96b3e7baf860
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2024-11-12 03:20:56 +0300

    Up httpx version

--- tests/it/test_entry.py
+++ tests/it/test_entry.py
@@ -88,7 +88,7 @@ def test(current_dir: Path) -> None:
     (_version_from_lock('attrs'),),
     ('attrs', '-U'),
 
-    ('httpx==0.20.0',),
+    ('httpx==0.23.0',),
     (_version_from_lock('httpx'),),
     ('httpx', '-U'),
 
