216e8dda39be207f8ab38c61e854e52030dabcab
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2025-08-13 20:38:53 +0300

    lint

---
+++ deltaver/fk_supports_str.py
@@ -0,0 +1,18 @@
+from typing import final
+
+import attrs
+
+from deltaver.supports_str import SupportsStr
+
+
+@final
+@attrs.define(frozen=True)
+class FkSupportsStr(SupportsStr):
+    """Fk supports str."""
+
+    _origin: str
+
+    def __str__(self) -> str:
+        """Return the version as a string."""
+        return self._origin
+   
--- deltaver/parsed_version.py
+++ deltaver/parsed_version.py
@@ -1,5 +1,3 @@
-"""Module for parsing version strings into semantic version objects."""
-
 # The MIT License (MIT).  #
 # Copyright (c) 2018-2025 Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
 #
@@ -21,12 +19,16 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 # OR OTHER DEALINGS IN THE SOFTWARE.
 
+"""Module for parsing version strings into semantic version objects."""
+
 from typing import final
 
 import attrs
 from semver import VersionInfo
 
 from deltaver.exceptions import InvalidVersionError
+from deltaver.fk_supports_str import FkSupportsStr
+from deltaver.supports_str import SupportsStr
 
 # Constants for version parsing
 MIN_VERSION_PARTS = 2
@@ -35,11 +37,14 @@ MIN_INDEX_FOR_PRE_RELEASE = 1
 
 @final
 @attrs.define(frozen=True)
-class DevVersionProcessor:
-    """Processor for dev versions."""
+class DevCorrectVersion(SupportsStr):
+    """Dev correct version."""
 
-    def process(self, origin: str) -> str:
-        """Handle dev versions: 0.13.dev0 -> 0.13.0-dev0, 0.13.0.dev1 -> 0.13.0-dev1."""
+    _origin: SupportsStr
+
+    def __str__(self) -> str:
+        """Return the version as a string."""
+        origin = str(self._origin)
         if '.dev' not in origin:
             return origin
         parts = origin.split('.dev')
@@ -54,59 +59,33 @@ class DevVersionProcessor:
 
 @final
 @attrs.define(frozen=True)
-class PreReleaseProcessor:
-    """Processor for pre-release versions."""
+class BetaCorrectVersion(SupportsStr):
+    """Beta correct version."""
 
-    def process_beta(self, origin: str) -> str:
-        """Process beta versions: 0.15.0b1 -> 0.15.0-b1."""
-        return self._process_single_char(origin, 'b', ['-b', '-beta'])
+    _origin: SupportsStr
 
-    def process_alpha(self, origin: str) -> str:
-        """Process alpha versions: 0.15.0a1 -> 0.15.0-a1."""
-        return self._process_single_char(origin, 'a', ['-a', '-alpha'])
-
-    def _process_single_char(self, origin: str, character: str, suffixes: list[str]) -> str:
-        """Process single character pre-release version."""
-        if character not in origin or origin.endswith(character):
+    def __str__(self) -> str:
+        """Return the version as a string."""
+        origin = str(self._origin)
+        if '-b' in origin or 'b' not in origin:
             return origin
-        if any(suffix in origin for suffix in suffixes):
+        parts = origin.split('b')
+        if len(parts) != MIN_VERSION_PARTS:
             return origin
-        return self._find_and_replace_character(origin, character)
-
-    def _find_and_replace_character(self, origin: str, character: str) -> str:
-        """Find and replace character with '-character'."""
-        origin_length = len(origin)
-        for index in range(origin_length):
-            is_valid = self._is_valid_position(origin, index, origin[index], character)
-            if is_valid:
-                prefix = origin[:index]
-                suffix = origin[index + 1:]
-                return f'{prefix}-{character}{suffix}'
-        return origin
-
-    def _is_valid_position(self, origin: str, index: int, current_char: str, character: str) -> bool:
-        """Check if position is valid for pre-release processing."""
-        if current_char != character or index <= 0:
-            return False
-        if index + 1 >= len(origin):
-            return False
-        prev_char_is_digit = origin[index - 1].isdigit()
-        next_char_is_digit = origin[index + 1].isdigit()
-        return prev_char_is_digit and next_char_is_digit
+        version_part, beta_number = parts[0], parts[1]
+        return f'{version_part}-b{beta_number}'
 
 
 @final
 @attrs.define(frozen=True)
-class RcAndPostProcessor:
-    """Processor for rc and post-release versions."""
+class PostCorrectVersion(SupportsStr):
+    """Post correct version."""
 
-    def process(self, origin: str) -> str:
-        """Process rc and post-release versions."""
-        # Handle rc versions: 0.15.0rc1 -> 0.15.0-rc1
-        rc_suffixes = ['-rc']
-        if 'rc' in origin and not any(suffix in origin for suffix in rc_suffixes):
-            origin = origin.replace('rc', '-rc')
-        # Handle post-release versions: 4.6.2.post1 -> 4.6.2+post1
+    _origin: SupportsStr
+
+    def __str__(self) -> str:
+        """Return the version as a string."""
+        origin = str(self._origin)
         if '.post' in origin:
             origin = origin.replace('.post', '+post')
         return origin
@@ -126,15 +105,17 @@ class ParsedVersion:
     def parse(self) -> VersionInfo:
         """Parse version."""
         origin = self._version.removeprefix('v')
-        # Process with different processors
-        dev_processor = DevVersionProcessor()
-        pre_release_processor = PreReleaseProcessor()
-        rc_post_processor = RcAndPostProcessor()
-        origin = dev_processor.process(origin)
-        origin = pre_release_processor.process_beta(origin)
-        origin = pre_release_processor.process_alpha(origin)
-        origin = rc_post_processor.process(origin)
         try:
-            return VersionInfo.parse(origin)
+            return VersionInfo.parse(
+                str(
+                    PostCorrectVersion(
+                        BetaCorrectVersion(
+                            DevCorrectVersion(
+                                FkSupportsStr(origin),
+                            ),
+                        ),
+                    ),
+                ),
+            )
         except ValueError as err:
             raise InvalidVersionError(self._version) from err
