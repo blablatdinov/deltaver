06ee4f9346381dbc8fe250d129ea978b0fb11704
Ilaletdinov Almaz <45946541+blablatdinov@users.noreply.github.com>
2026-08-11 10:08:31 +0300

    Fix ruff warnings (#282)

--- deltaver/_internal/config.py
+++ deltaver/_internal/config.py
@@ -3,8 +3,6 @@
 
 """Config dict."""
 
-# noqa: FOC100. Data structures
-
 from __future__ import annotations
 
 from pathlib import Path
--- deltaver/_internal/freezed_reqs.py
+++ deltaver/_internal/freezed_reqs.py
@@ -20,7 +20,8 @@ class FreezedReqs(ParsedReqs):
     _requirements_file_content: str
 
     @override
-    def reqs(self) -> list[tuple[str, str]]:  # noqa: WPS210. Simplify later
+    # TODO: fix
+    def reqs(self) -> list[tuple[str, str]]:  # noqa: WPS210, WPS231
         """Parsed requirements list."""
         res = []
         lines = self._requirements_file_content.splitlines()
